ea5cd4684097a5107c6c85f81aca7b4f4fe170e1
Yogesh25112000 <yogesh3@dcmail.ca>
2025-05-10 20:54:47 -0400

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,55 @@
+age = 29
+
+if age >=18:
+    print("you can drive")
+elif age >= 30:
+    print("you can drive and apply for passport")
+else:
+    print("you are a teenager")
+
+num = 24
+
+if num %2 == 0:
+    print("its even")
+else:
+    print("its odd")
+
+marks = 92
+
+if marks >= 90:
+    print("grade is A")
+elif marks >=80 and marks <=90:
+    print("grade is B")
+elif marks >=70 and marks <=80:
+    print(" grade is C")
+else:
+    print("fail")
+
+price = 1001
+if price <= 1000:
+    print("you can buy")
+else:
+    print("you cannot buy")
+
+    
+my_tuple = (1, 2, 3, 4, 5)
+my_list = list(my_tuple)
+my_list.append(6)
+my_list.remove(2)
+new_tuple = tuple(my_list)
+print("Updated tuple:", new_tuple)
+
+
+result = math.sqrt(16)
+print(result)  
+print(math.pi)  
+print("hello Yogi ")
+
+a= 5
+b = 10
+print(a+b)
+
+Name = "Yogesh"
+Age = 25
+is_student = True
+
